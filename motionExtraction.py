2bc202e11aa141200cb2a0a2aa63d9fa8a699636
Caleb L <degogolin@gmail.com>
2024-03-10 23:29:00 -0700

    somewhat working

--- motionExtraction.py
+++ motionExtraction.py
@@ -1,15 +1,132 @@
-from PIL import Image
 import cv2
+import numpy as np
+import traceback
+import random
+import json
 
-video = cv2.VideoCapture(r'video2.mp4')
+#
+sameThresh = 0.5
+varianceThresh = 0.1
+
+tClose = 3
+tThresh = 7
+
+rThresh = 10
+
+frameDiff = 1
+frameskip = 100
+moveThresh = 40
+robotThresh = 10
+robotSize = 50
+showSize = (540, 380)
+prevFrames = []
+
+maxContours = 15
+#
+
+video = cv2.VideoCapture(r'Final Tiebreaker - 2024 Silicon Valley Regional.mp4')
+ret, frame = video.read()
+rows, cols, ch = frame.shape
+
+idAreas = np.zeros((rows, cols, 3), dtype = np.uint8) #(index base 255)
+tAreas = np.zeros((rows, cols), dtype = np.float64)
+paths = np.zeros((rows, cols, 3), dtype = np.uint8)
+maxT = tThresh - tClose
+class FieldObject():
+    objNum = 1
+    objects = []
+
+    def __init__(self, initPoint, radius, t):
+        self.id = FieldObject.objNum
+        self.color = (self.id//(255*255), self.id//255, self.id % 255)
+        self.randColor = (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
+        FieldObject.objNum += 1
+
+        self.path = []
+
+        FieldObject.objects.append(self)
+        self.addPoint(initPoint, radius, t)
+
+    def addPoint(self, point, radius, t):
+        if len(self.path) > 0:
+            cv2.line(paths, self.path[-1][0], point, self.randColor, thickness = 2)
+
+        cv2.circle(idAreas, point, r, self.color, cv2.FILLED)
+        cv2.circle(tAreas, point, r, t, cv2.FILLED)
+        self.path.append((point, radius, t))
+
+    def getObject(point, radius, t):
+        if radius < 5:
+            return None
+        
+        area = idAreas[(point[1] - radius) : (point[1] + radius), (point[0] - radius) : (point[0] + radius)]
+
+        if area.size == 0:
+            return None
+
+        size = area.size//3
+        area = np.reshape(area, (size, 3))
+
+        tArea =  tAreas[(point[1] - radius) : (point[1] + radius), (point[0] - radius) : (point[0] + radius)] - (t - tThresh)
+        tArea[tArea < 0] = 0
+        tArea[tArea > tClose] = tClose
+        tArea = np.reshape(tArea, (size,))
+
+        # tArea = 
+        # area = area[(area).sum(axis = 1) > 0]
+
+        # nonZero = area.size//3
+
+        # # print(nonZero, size)
+        # if nonZero/size < 0.1:
+        #     return 0
+
+        #average pixels within radius
+
+        if np.sum(tArea) == 0:
+            return 0
+
+        avgCol = np.average(area, weights = tArea, axis = 0)
+        stdCol = np.average((area - avgCol)**2, weights = tArea, axis = 0)
+
+        avg = avgCol[0]*(255*255) + avgCol[1]*255 + avgCol[2]
+        std = stdCol[0]*(255*255) + stdCol[1]*255 + stdCol[2]
+
+        #print(avg, std)
+
+        if std > varianceThresh:
+            return None
+        if 0.5 - abs((avg % 1.0) - 0.5) < sameThresh:
+            id = round(avg)
+            if id == 0:
+                return 0
+            
+            o = FieldObject.objects[id - 1]
+            p, r, t = o.path[-1]
+            if abs(r - radius) > rThresh:
+                return 0
+
+            dx = point[0]-p[0]
+            dy = point[1]-p[1]
+            d = (dx*dx) + (dy*dy)
+            if d < min(radius*radius, r*r):
+                return o
+            else:
+                return None
+        return None
+        
+objects = []
 
 try:
-    frameDiff = 1
-    prevFrames = []
+    n = 0
     while video.isOpened():
         ret, frame = video.read()
+        n += 1
         if not ret:
             break
+
+        if n < frameskip:
+            continue
 
         if len(prevFrames) < frameDiff:
             prevFrames.append(frame)
@@ -22,22 +139,71 @@ try:
 
         filter = cv2.absdiff(frame, prevFrame)
 
-        filter = cv2.resize(filter, (540, 380), fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC)
-        #cv2.imshow('Frame', frame)
-        cv2.imshow("filter", filter)
+        gray = cv2.cvtColor(filter, cv2.COLOR_BGR2GRAY)
+        _, mask = cv2.threshold(gray, moveThresh, 255, cv2.THRESH_BINARY)
+
+        masked = cv2.bitwise_and(frame, filter, mask = mask)
+        integral = np.float32(cv2.integral(mask))
+
+        integral1 = cv2.warpAffine(integral, np.float32([[1,0,0],[0,1,robotSize]]), (cols + 1, rows + 1))
+        integral2 = cv2.warpAffine(integral, np.float32([[1,0,robotSize],[0,1,0]]), (cols + 1, rows + 1))
+        integral3 = cv2.warpAffine(integral, np.float32([[1,0,robotSize],[0,1,robotSize]]), (cols + 1, rows + 1))
+        vals = integral - integral1 - integral2 + integral3
+
+        diff = (vals/(robotSize*robotSize)).astype('uint8')
+        diff = cv2.warpAffine(diff, np.float32([[1,0,-robotSize/2.0],[0,1,-robotSize/2.0]]), (cols, rows))
+
+        _, robMask = cv2.threshold(diff, robotThresh, 255, cv2.THRESH_BINARY)
+
+        contours, heirarchy = cv2.findContours(image = robMask, mode = cv2.RETR_TREE, method = cv2.CHAIN_APPROX_NONE)
+
+        #out = cv2.bitwise_and(frame, frame, mask = robMask)
+        #cv2.drawContours(image = out, contours = contours, contourIdx = -1, color = (0,255,0), thickness = 2)
+
+        if len(contours) > maxContours:
+            continue
+
+        out = idAreas.copy()
+
+        cv2.addWeighted(paths, 1, out, 1, 0, out)
+        #out[maskLine] = paths
+
+        for c  in contours:
+            if len(c) > 1000:
+                continue
+
+            center = cv2.mean(c)[:2]
+            center = (int(center[0]), int(center[1]))
+            dist = (c-center)
+            r = int(np.mean(np.sqrt((dist*dist).sum(axis = 2))))
+
+            obj = FieldObject.getObject(center, r, n)
+            if obj is None:
+                pass
+            elif obj == 0:
+                FieldObject(center, r, n)
+            else:
+                obj.addPoint(center, r, n)
+
+            cv2.circle(out, center, r, (0,255,0), thickness = 2)
+
+        out = cv2.resize(out, showSize, fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC)
+        frame = cv2.resize(frame, showSize, fx = 100, fy = 0, interpolation = cv2.INTER_CUBIC)
+        cv2.imshow("original", frame)
+        cv2.imshow("filter", out)
 
         if (cv2.waitKey(25) & 0xFF) == ord('q'):
             break
-except:
+    
+except Exception as error:
+    traceback.print_exc()
     pass
-
-video.release()
-cv2.destroyAllWindows()
-
-# newVideo = None
-# frameDiff = offset * fps
-# for i in range(0, numFrames - frameDiff):
-#     frame = video[i] - video[i+frameDiff]
-#     newVideo.add(frame)
-
-# newVideo.save()
+finally:
+    cv2.imwrite("paths.png", paths) 
+    video.release()
+    cv2.destroyAllWindows()
+    with open("paths.json", "w") as file:
+        json_str = json.dumps({p.id : p.path for p in FieldObject.objects})
+        json_str = json_str.replace(', "', ',\n "')
+        file.write(json_str)
+        #json.dump(json.load()
---
+++ motionGrapher.py
@@ -0,0 +1,111 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import random
+
+filename = "paths copy.json"
+
+with open(filename, "r") as file:
+    data = json.load(file)
+
+width = 1280
+height = 720
+
+addP = lambda p1, p2 : (p1[0] + p2[0], p1[1] + p2[1])
+subP = lambda p1, p2 : (p1[0] - p2[0], p1[1] - p2[1])
+divP = lambda p, k : (p[0]/k, p[1]/k)
+mulP = lambda p, k : (p[0]*k, p[1]*k)
+dotP = lambda p1, p2 : p1[0]*p2[0] + p1[1]*p2[1]
+pMagn = lambda p : (p[0]*p[0] + p[1]*p[1])**(0.5)
+
+trapezoid = [
+    (409, 189),
+    (850, 188),
+    (1100, 372),
+    (152, 372)
+]
+
+tTop = subP(trapezoid[1], trapezoid[0])[0]
+tBot = subP(trapezoid[2], trapezoid[3])[0]
+tHeight = subP(trapezoid[0], trapezoid[3])[1]
+
+fieldWidth = 16.5
+fieldHeight = 8.25
+
+count = 0
+newPaths = []
+tMax = 0
+for id, path in data.items():
+    if len(path) < 5:
+        continue
+
+    cx = [p[0][0] for p in path]
+    cy = [p[0][1] for p in path]
+    dx = max(cx) - min(cx)
+    dy = max(cy) - min(cy)
+    if dx*dx + dy*dy < 50*50:
+        continue
+
+    x = []
+    y = []
+    newPath = []
+    for p, r, t in path:
+        x.append(p[0])
+        y.append(p[1])
+
+        p2 = subP(p, trapezoid[3])
+        yScale = fieldHeight/tHeight
+        ry = fieldHeight - (p2[1] + r*0.3) * yScale #offset robot by a bit of the radius
+        percent = (tHeight - ry)/tHeight
+        xScale = fieldWidth / (percent*tBot + (1.0-percent)*tTop)
+    
+        rx = p2[0] * xScale
+
+        if t > tMax:
+            tMax = t
+
+        if rx>fieldWidth or rx<0 or ry>fieldHeight or ry<0:
+            continue
+        
+        newPath.append(((rx,ry), t))
+
+    newPaths.append(newPath)
+    plt.plot(x,y)
+
+    count += 1
+
+plt.show()
+
+t = 0
+
+video = cv2.VideoCapture(r'Match 1 (R1) - 2024 Hueneme Port Regional.mp4')
+
+field = cv2.imread("2024Game2.png") 
+imgHeight, imgWidth, _ = field.shape
+scale = imgWidth/fieldWidth
+
+timeDiff = 30
+while t < tMax:
+    paths = np.zeros((imgHeight, imgWidth, 3), dtype=np.uint8)
+    for path in newPaths:
+        lastP = None
+        for point, pt in path:
+            if pt > t - timeDiff and pt < t:
+                p = mulP(point, scale)
+                p = (int(p[0]), int(p[1]))
+                if lastP is not None:
+                    cv2.line(paths, lastP, p, (255,0,0), thickness = 2)
+                lastP = p
+    
+    
+    ret, frame = video.read()
+    frame = cv2.resize(frame, (540, 380), fx = 100, fy = 0, interpolation = cv2.INTER_CUBIC)
+
+    cv2.imshow("paths", cv2.addWeighted(paths, 1, field, 1, 0))
+    cv2.imshow("video", frame)
+    t+=1
+    if (cv2.waitKey(25) & 0xFF) == ord('q'):
+        break
+
+cv2.destroyAllWindows()
